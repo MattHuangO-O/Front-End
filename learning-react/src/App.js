3fc5b0914f50a40a6da7fbb9bcc94fa91585af83
Matthew Huang <19huangm@gmail.com>
2024-04-28 18:55:45 -0400

    learned about components and how to work on dev server

---
+++ learning-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './Components/HelloWorld'
+import Counter from './Components/CounterExample';
+
+function App() {
+  return (
+    <div>
+      {/*<HelloWorld name == "Matt"/>*/} 
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ learning-react/src/Components/CounterExample.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Counter(){
+    //const [count, setCount] = useState(0)
+
+    console.log(useState(0))
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Counter
